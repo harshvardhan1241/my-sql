2f8fe4907920e7ef91189183a901d134ef77e1eb
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2026-09-19 17:16:53 +0530

    Add new queries to cte.sql and create practice_problem_subquer&cte_1.sql

--- cte.sql
+++ cte.sql
@@ -6,3 +6,21 @@ WITH job_january AS (
 SELECT * FROM job_january;
 
 
+select*
+from job_postings_fact
+
+
+with company_job_count as (
+  select
+   company_id,
+    count(*) as job_count
+  from job_postings_fact
+  group by company_id
+)
+
+select
+ company_dim.name as company_name,
+company_job_count.job_count
+from company_dim
+left join company_job_count ON company_job_count.company_id = company_dim.company_id
+order by company_job_count.job_count desc
