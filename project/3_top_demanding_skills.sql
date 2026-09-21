4031559d301f68921e143a9e61fc9ed9b95421df
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2026-09-21 20:11:01 +0530

    Add SQL queries to analyze top-paying Data Analyst jobs and in-demand skills

---
+++ project/3_top_demanding_skills.sql
@@ -0,0 +1,31 @@
+/*
+Question: What are the most in-demand skills for data analysts?
+-Join job postings to inner join table similar to query 2
+-Identify the top 5 in-demand skills for a data analyst.
+-Focus on all job postings.
+-Why? Retrieves the top 5 skills with the highest demand in the job market,
+providing insights into the most valuable skills for job seekers.
+*/
+
+SELECT
+skills,
+count(skd.job_id) as demand_count
+FROM 
+job_postings_fact as jpf
+INNER JOIN 
+skills_job_dim as skd
+ON
+jpf.job_id=skd.job_id
+INNER JOIN
+skills_dim AS sd
+ON
+sd.skill_id = skd.skill_id
+WHERE
+jpf.job_title_short ='Data Analyst'
+AND
+jpf.job_work_from_home = TRUE
+GROUP BY
+skills
+ORDER BY
+demand_count DESC
+LIMIT 5
