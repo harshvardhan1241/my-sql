5ab839ca69223ba49d8d29dc8e9f3e318bc64bae
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2026-09-24 02:32:06 +0530

    Fix data type errors in branch, employees, and books tables; update column sizes for branch_address, contact_no, and category

--- project/porfolio project 2/app_library.sql
+++ project/porfolio project 2/app_library.sql
@@ -10,6 +10,17 @@ branch_address VARCHAR(10),
 contact_no VARCHAR(10)
 )
 
+--late changes due to error 
+-- error in the branch_address  var vlues is befre is 10 now we are making it 40
+
+ALTER TABLE branch
+ALTER branch_address type varchar(40)
+
+--error in the  contact_no also var value is geting less and so ve arew making it 20
+
+ALTER TABLE branch
+ALTER contact_no type varchar(20)
+
 --create tabel for employees
 DROP TABLE if EXISTS employees;
 CREATE table employees(
@@ -19,6 +30,12 @@ CREATE table employees(
     salary int,
     branch_id varchar(25) --FOREIGN KEY 
 )
+
+--late changes due to error 
+-- error in the salary due to not proper data type
+
+ALTER TABLE employees
+ALTER salary type float
 
 --create tabel for books
 DROP TABLE if EXISTS books;
@@ -31,6 +48,11 @@ status varchar(15),
 author varchar(35),
 publisher varchar(55)
 )
+--late changes due to error 
+-- error is the category var vlue is befre is 10 now we are making it 20
+ALTER TABLE books
+ALTER category type varchar(20)
+
 
 --create tabel for members
 DROP TABLE if EXISTS members;
@@ -96,4 +118,8 @@ ADD CONSTRAINT  fk_issued_status
 FOREIGN KEY (issued_id)
 REFERENCES  issued_status (issued_id)
 
---info inserting
+/*ERROR: insert or update on table "return_status" violates foreign key constraint "fk_issued_status"
+DETAIL: Key (issued_id)=(IS101) is not present in table "issued_status".
+psql: error: utility failed with exit code: 1
+
+so we delete that colum of innfo from csv file 
