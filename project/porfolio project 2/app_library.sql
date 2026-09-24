61c50e24492dfe68009078d4515a4120cb1dd7ed
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2026-09-25 01:50:33 +0530

    Add SQL tasks: implement query for books not yet returned, insert issued books, and update return status with book quality

--- project/porfolio project 2/app_library.sql
+++ project/porfolio project 2/app_library.sql
@@ -397,3 +397,56 @@ FROM
 rental_price_of_each_book
 
 -- Task 12: Retrieve the List of Books Not Yet Returned
+
+SELECT
+ist.issued_book_name as not_return_book,
+ist.issued_member_id,
+mbr.member_name
+FROM
+issued_status as ist
+LEFT JOIN
+return_status as rst 
+on ist.issued_id=rst.issued_id
+LEFT JOIN
+members as mbr
+ON
+ist.issued_member_id=mbr.member_id
+where 
+rst.return_id is NULL
+
+
+-- INSERT INTO book_issued in last 30 days
+-- SELECT * from employees;
+-- SELECT * from books;
+-- SELECT * from members;
+-- SELECT * from issued_status
+
+
+INSERT INTO issued_status(issued_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id)
+VALUES
+('IS151', 'C118', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL '24 days',  '978-0-553-29698-2', 'E108'),
+('IS152', 'C119', 'The Catcher in the Rye', CURRENT_DATE - INTERVAL '13 days',  '978-0-553-29698-2', 'E109'),
+('IS153', 'C106', 'Pride and Prejudice', CURRENT_DATE - INTERVAL '7 days',  '978-0-14-143951-8', 'E107'),
+('IS154', 'C105', 'The Road', CURRENT_DATE - INTERVAL '32 days',  '978-0-375-50167-0', 'E101');
+
+-- Adding new column in return_status
+
+ALTER TABLE return_status
+ADD Column book_quality VARCHAR(15) DEFAULT('Good');
+
+UPDATE return_status
+SET book_quality = 'Damaged'
+WHERE issued_id 
+    IN ('IS112', 'IS117', 'IS118');
+SELECT * FROM return_status;
+
+
+/*
+### Advanced SQL Operations
+
+Task 13: Identify Members with Overdue Books
+Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's name, book title, issue date, and days overdue.
+*/
+
+
+
